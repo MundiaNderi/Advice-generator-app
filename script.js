d3e928dfcc6c5ec4f771aac84c1173e24dd1f841
Serah Nderi <sarahngima77@gmail.com>
2023-11-16 21:15:27 +0300

    Fixing quote id

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const quoteElement = document.querySelector('.quote q');
+  const adviceIdElement = document.querySelector('.id');
 
-  function fetchAndDisplayQuote() {
+  function fetchAndUpdateQuote() {
     fetch('https://api.adviceslip.com/advice')
       .then(response => response.json())
       .then(data => {
+        adviceIdElement.textContent = data.slip.id;
         quoteElement.textContent = data.slip.advice;
       })
       .catch(error => {
@@ -12,13 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   }
 
-  // Initial quote on page load
-  fetchAndDisplayQuote();
+  // Initial quote and ID on page load
+  fetchAndUpdateQuote();
 
   // Get the dice button element
   const Button = document.getElementById('Button');
 
   // Event listener for clicking the dice
-  Button.addEventListener('click', fetchAndDisplayQuote);
+  Button.addEventListener('click', fetchAndUpdateQuote);
 });
-
